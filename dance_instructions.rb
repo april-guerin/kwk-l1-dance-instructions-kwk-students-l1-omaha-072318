090187711dcaaefb87e4e9f7710413f40aa27d8c
april-guerin <aprilguerin@yahoo.com>
2018-07-23 21:01:18 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -6,12 +6,6 @@ def starting_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
 end
 
-def base_footwork
-  puts "lift right foot"
-  puts "return right foot to the ground"
-  puts "finishing with a small skip-step backward"
-end
-
 #skip-step
 def skip_step("left")
   puts "skip a step (lower, bounce, briefly)"

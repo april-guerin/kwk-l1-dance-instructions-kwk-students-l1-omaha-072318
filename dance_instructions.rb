f6a206f639dffb79053c108d92b73c83d181bd51
april-guerin <aprilguerin@yahoo.com>
2018-07-23 21:01:57 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -7,7 +7,20 @@ def starting_stance
 end
 
 #skip-step
-def skip_step()
+def skip_step("left")
   puts "skip a step (lower, bounce, briefly)"
 end
 
+#Upper-body
+
+def bob_the_reins
+  puts "holding your arms out in front of you, straight and at chest level"
+  puts "cross your right wrist over your left and hold them together"
+  puts "lift your arms up and down in a loose bouncing movement in time with the beat of the song. This movement is repeated eight time"
+end
+
+def lasso
+  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "lift right arm up and point right elbow diagonally to the right"
+  puts "Raise right forearm so that it points straight up, and whip it in small circles to the beat of the song, as if you were a cowboy holding a rope lasso. This movement is repeated eight times as well."
+end

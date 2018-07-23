cff154b8a11dbeb1075c2ce9e27b6a7406d8f7e9
april-guerin <aprilguerin@yahoo.com>
2018-07-23 20:57:33 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -1,6 +1,39 @@
 
 # Code your methods below
 
+#Lower Body
 def proper_stance
   puts "Plant legs far apart, bend knees slightly and keep posture loose."
 end
+
+def base_footwork
+  puts "lift right foot"
+  puts "return right foot to the ground"
+  puts "finishing with a small skip-step backward"
+end
+
+#skip-step
+def skip_step_left
+  puts "lower left foot to the ground"
+  puts "bounce left foot back up slightly, kicking it a few inches back"
+  puts "left, right, left and left"
+end
+
+def skip_step_right
+  puts "lower right foot to the ground"
+  puts "bounce right foot back up slightly, kicking it a few inches back"
+  puts "right, left, right and right"
+end
+
+#Upper-body
+
+def bob_the_reins
+  puts "holding your arms out in front of you, straight and at chest level"
+  puts "cross your right wrist over your left and hold them together"
+  puts "lift your arms up and down in a loose bouncing movement in time with the beat of the song. This movement is repeated eight time"
+end
+
+def lasso
+  puts "start arm movement by holding left arm so the back of your wrist is near your chin, pointing your left elbow straight left."
+  puts "lift right arm up and point right elbow diagonally to the right"
+  puts ""

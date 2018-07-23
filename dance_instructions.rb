5a783f80f756f9962a67009d89501b70346bd1e2
april-guerin <aprilguerin@yahoo.com>
2018-07-23 21:11:25 +0000

    automatically backed up by learn

--- dance_instructions.rb
+++ dance_instructions.rb
@@ -19,4 +19,5 @@ end
 
 def bob_the_reins
   puts "Holding your arms out in front of you, straight and at chest level"
-  
+  puts "Cross your right wrist over your left and hold them together"
+  puts ""
